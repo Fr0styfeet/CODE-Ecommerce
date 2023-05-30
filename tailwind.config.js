9ff99b8e084863bd22c01276e15aa67096668a13
Anisha <anshumankumarlal@gmail.com>
2023-05-30 18:08:55 +0530

    first slide done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#4A99D3',
+      },
+    },
+  },
+  plugins: [],
+}
+
