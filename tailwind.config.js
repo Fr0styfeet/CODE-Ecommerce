0783bc22837be830dff4083cec9a65fd4c6f1e0f
Anisha <anshumankumarlal@gmail.com>
2023-05-30 19:25:50 +0530

    second slide almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       colors: {
         customBlue: '#4A99D3',
       },
+      boxShadow: {
+        'lol': '5px 20px 50px rgba(74, 153, 211, 0.2)',
+      }
     },
   },
   plugins: [],
